b0ba3f4bb6cce09afd47e4fa092fcd5a465802c4
prakashbalab24 <prakashbala.b24@gmail.com>
2017-10-13 17:53:11 +0530

    second anim added

--- app/src/main/java/singledev/lottie/MainActivity.java
+++ app/src/main/java/singledev/lottie/MainActivity.java
@@ -2,12 +2,39 @@ package singledev.lottie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+    private RelativeLayout layout;
+    private boolean changeFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.parent_layout);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(changeFlag) {
+                    changeAnimation("data.json");
+                    changeFlag = false;
+                }
+                else {
+                    changeAnimation("newsanim.json");
+                    changeFlag = true;
+                }
+            }
+        });
+    }
+
+    private void changeAnimation(String anim) {
+        LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        animationView.setAnimation(anim);
+        animationView.loop(true);
+        animationView.playAnimation();
     }
 }
